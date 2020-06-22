cc457769aceeae1d4a3c3bbc5a8ac7881fd061f3
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-06-22 14:06:44 +0300

    Write func for button cart and correct component header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logoSvg from './assets/img/pizza-logo.svg';
+import Button from './Button'
 
 function Header() {
   return (
@@ -13,8 +14,8 @@ function Header() {
           </div>
         </div>
         <div className="header__cart">
-          <a href="/cart.html" className="button button--cart">
-            <span>520 ₴</span>
+          <Button className= "button--cart">
+          <span>520 ₴</span>
             <div className="button__delimiter"></div>
             <svg
               width="18"
@@ -45,7 +46,8 @@ function Header() {
               />
             </svg>
             <span>3</span>
-          </a>
+          </Button>
+
         </div>
       </div>
     </div>
