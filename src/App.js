fb86a45497734fe6299e54657632f250300b4482
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-06-22 15:25:57 +0300

    Add folder components,and components <Categiries>,System-Import.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
-
-import Header from './Header';
-import Button from './Button';
+import {Header,Categories} from './components';
 
 function App() {
   return (
     <div className="wrapper">
       <Header/>
-
-
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories/>
             <div className="sort">
               <div className="sort__label">
                 <svg
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Button from './Button';
+import Header from './Header';
+import Categories from './Categories';
+
+export {Button,Header,Categories};
