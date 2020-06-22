02eb491361c6b190194cf22667981af0b5edb368
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-06-22 15:53:47 +0300

    Add map for render li-list for categories.

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Categories() {
+function Categories({items}) {
+    console.log(items)
     return (
-                     <div className="categories">
+             <div className="categories">
               <ul>
                 <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {
+                 items.map((name, index) => 
+                ( <li key={`${name}_${index}`}>{name}</li> ))
+                }
               </ul>
             </div>       
     )
