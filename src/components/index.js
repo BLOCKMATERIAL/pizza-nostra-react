901ca36088e012f372edae6b4cc5640e066347a5
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-06-24 22:11:27 +0300

    Added new component <Sort>

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Button from './Button';
 import Header from './Header';
 import Categories from './Categories';
+import SortPopup from './SortPopup';
 
-export {Button,Header,Categories};
+export {Button,Header,Categories,SortPopup};
