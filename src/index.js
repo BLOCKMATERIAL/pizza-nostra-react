614060b32ee9da90a71cf930c7ea05d0310b26ae
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-07-01 00:04:12 +0300

    Added react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter,Route} from 'react-router-dom';
 import './scss/app.scss';
 
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root'),
 );
