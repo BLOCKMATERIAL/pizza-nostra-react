babe9a2c7d9149a94b2bc9c880e3369e127b68f3
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-07-01 15:49:43 +0300

    Added new component PizzaBlock.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import {Home,Cart} from './pages';
 import { Route } from 'react-router-dom';
 
 function App() {
+
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json').then((resp) => resp.json()).then(json => {
+    setPizzas(json.pizzas);
+    });
+    
+  }, [])
+
+  console.log(pizzas);
+
   return (
     <div className="wrapper">
       <Header/>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Button from './Button';
 import Header from './Header';
 import Categories from './Categories';
 import SortPopup from './SortPopup';
+import PizzaBlock from './PizzaBlock';
 
-export {Button,Header,Categories,SortPopup};
+
+export {Button,Header,Categories,SortPopup,PizzaBlock};
