1c7bc99f7f2f3161665da7aec6fbf8b503b63fd8
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-07-02 17:19:08 +0300

    Added library Axios and delete fetch request.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import axios from 'axios';
 import {Header} from './components';
 import {Home,Cart} from './pages';
 import { Route } from 'react-router-dom';
@@ -9,9 +9,9 @@ function App() {
   const [pizzas, setPizzas] = React.useState([]);
 
   React.useEffect(() => {
-    fetch('http://localhost:3000/db.json').then((resp) => resp.json()).then(json => {
-    setPizzas(json.pizzas);
-    });
+    axios.get('http://localhost:3000/db.json').then(({data}) => {
+    setPizzas(data.pizzas);
+    })
     
   }, [])
 
@@ -21,7 +21,7 @@ function App() {
     <div className="wrapper">
       <Header/>
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} /> } exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
