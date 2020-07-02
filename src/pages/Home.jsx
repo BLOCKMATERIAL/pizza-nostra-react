d1f468cf9ea7b82fb27f3e2276f7923e8169acad
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-07-02 17:23:33 +0300

    Replaced method for trasfer object to component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import {Categories,SortPopup, PizzaBlock} from '../components';
 
-function Home() {
+function Home({items}) {
     return (
         <div className="container">
           <div className="content__top">
@@ -11,7 +11,9 @@ function Home() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock/>
+            {items.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj}/>
+            ))}
           </div>
         </div>
     )
