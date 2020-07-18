85b6cef9f6fe45b251a0f0947fb790750c8275bd
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-07-18 17:57:42 +0300

    Added Redux and creacte store for testing.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './scss/app.scss';
-
+import {createStore} from 'redux'
 import App from './App';
+
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+const store = createStore(counter);
+
+store.subscribe( () => console.log ('хранилище измн', store.getState()));
+
+console.log(store.getState());
+store.dispatch({ type : 'INCREMENT'});
 
 ReactDOM.render(
 
- 
   <Router>
     <App/>
   </Router>,
