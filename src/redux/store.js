547151fe1454e4dab93ae3efa74b3a87d0dbcf62
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-07-30 22:13:15 +0300

    Added composeEnhancers.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
-import { createStore} from 'redux';
-import rootReducer from './reducers'
-
-const store = createStore(rootReducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import { createStore,compose,applyMiddleware} from 'redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
 
-window.store = store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+
+
 export default store;
