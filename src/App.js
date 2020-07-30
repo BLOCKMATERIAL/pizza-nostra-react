5333f742a5bfa543cd04659d788a77edd6a6b1b6
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-07-30 21:31:30 +0300

    Change axios get request.

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
   const dispatch = useDispatch(); 
 
   React.useEffect(() => {
-    axios.get('http://localhost:3000/db.json').then(({data}) => {
-      dispatch(setPizzas(data.pizzas));
+    axios.get('http://localhost:3001/pizzas').then(({data}) => {
+      dispatch(setPizzas(data));
     }); 
 
 }, []);
