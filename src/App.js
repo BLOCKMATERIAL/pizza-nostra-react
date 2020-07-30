aa0213ab734f9cc6993e50e774712679db35e6e6
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-07-30 22:17:01 +0300

    Delete get request.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,15 @@ import { useDispatch } from 'react-redux';
 import {Header} from './components';
 import {Home,Cart} from './pages';
 import { Route } from 'react-router-dom';
-import {setPizzas} from './redux/actions/pizzas';
+import {fetchPizzas} from './redux/actions/pizzas';
 
 
 function App() {
   const dispatch = useDispatch(); 
 
   React.useEffect(() => {
-    axios.get('http://localhost:3001/pizzas').then(({data}) => {
-      dispatch(setPizzas(data));
-    }); 
+
+    (dispatch(fetchPizzas()))
 
 }, []);
 
